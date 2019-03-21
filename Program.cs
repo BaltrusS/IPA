6e7ff3c1153dc42bcea6360e2d60f76aced8992b
Baltrus <b.skridaila@gmail.com>
2019-03-22 01:10:08 +0200

    First code v0.1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace IPA
 {
@@ -7,12 +7,42 @@ namespace IPA
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<double> ndResults = new List<double>();
+			double examResult;
+			string vardas="Name", pavarde="Name";
 			
-			// TODO: Implement Functionality Here
+			Console.Write("Iveskite varda: ");
+			vardas = Console.ReadLine();
+			Console.Write("Iveskite pavarde: ");
+			pavarde = Console.ReadLine();
+			
+			Console.Write("Iveskite kiek turite nd rezultatu: ");
+			int n = int.Parse(Console.ReadLine());
+			for (int i=0; i<n; i++) {
+				Console.Write("Iveskite namu darbu rezultata nr "+(i+1)+": ");
+				ndResults.Add(Double.Parse(Console.ReadLine()));
+			}
+			Console.WriteLine(avg(ndResults)*0.3);
+			Console.Write("Iveskite egzamino rezultata: ");
+			examResult = Double.Parse(Console.ReadLine());
+			
+			double finalGrade = ((avg(ndResults)*0.3) + (examResult*0.7));
+			
+			Console.WriteLine("-------------------------------------------------------");
+			Console.WriteLine("First Name        | Last Name           |   Final grade");
+			Console.WriteLine("-------------------------------------------------------");
+			Console.WriteLine(String.Format("{0,-20}{1,-20}{2,15:0.00}", vardas, pavarde, finalGrade));
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
+		}
+		
+		public static double avg(List<double> list) {
+			double sum=0;
+			foreach (double grade in list) {
+				sum=sum+grade;
+			}
+			return sum/list.Count;
 		}
 	}
 }
